5be59f0b2e01060277e2fb3bcff2984ad69a3894
k13wk4 <gaydilo1995@gmail.com>
2025-01-16 18:11:14 +0200

    qq

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -691,7 +691,7 @@ class Tapper:
                                         break
 
                                     # Получаем новый уровень героя
-                                    new_level =  hero_lvl_up.get('data', {}).get('hero', {}).get('level', {})
+                                    new_level =  hero_lvl_up.get('data', {}).get('hero', {}).get('level')
 
                                     if new_level is not None:
                                         hero['level'] = new_level
